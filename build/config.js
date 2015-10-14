426562d96fab4bc7008613b9bb8668ab201c0528
Dmitry Vibe <riim@yandex.ru>
2015-10-14 12:18:00 +0600

    fixes

--- build/config.js
+++ build/config.js
@@ -1,6 +1,25 @@
+var src = 'src';
+var dist = 'dist';
+
 var config = {
-	src: 'src',
-	dist: 'dist'
+	src: src,
+	dist: dist,
+
+	styles: {
+		src: [
+			src + '/base.css',
+			src + '/textfield.css',
+			src + '/select.css',
+			src + '/box.css',
+			src + '/button.css',
+			src + '/hint.css',
+			src + '/tooltip.css',
+			src + '/preloader.css',
+			src + '/grid.css'
+		],
+
+		outputName: 'ceres'
+	}
 };
 
 module.exports = config;
--- build/tasks/build.js
+++ build/tasks/build.js
@@ -6,18 +6,8 @@ var $ = require('gulp-load-plugins')();
 var config = require('../config');
 
 gulp.task('build', function() {
-	return gulp.src([
-		config.src + '/base.css',
-		config.src + '/textfield.css',
-		config.src + '/select.css',
-		config.src + '/box.css',
-		config.src + '/button.css',
-		config.src + '/hint.css',
-		config.src + '/tooltip.css',
-		config.src + '/preloader.css',
-		config.src + '/grid.css'
-	])
-		.pipe($.concat('ceres.css'))
+	return gulp.src(config.styles.src)
+		.pipe($.concat(config.styles.outputName + '.css'))
 		.pipe($.postcss([
 			postcssSimpleVars,
 			autoprefixer({ browsers: ['last 2 version', '> 1%'] })
